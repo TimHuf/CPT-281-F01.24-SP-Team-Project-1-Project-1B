b5e55e19193eef6a3054b1ece2a68ae518024ac2
KatieD4 <162634216+KatieD4@users.noreply.github.com>
2024-03-07 20:25:41 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 
-#include "PolynominalList.h"
+#include "PolynomialList.h"
 #include <iostream>
 
 int main()
 {
-    cout << "Polynominal Calcultor\n";
+    cout << "Polynomial Calculator\n";
 
     // Variables used for first and second polynominal, and the final result and user choice
     Polynominal poly_1, poly_2, result;
@@ -12,33 +12,33 @@ int main()
     // Displays the menu for user to navigate through
     while (true) {
         cout << "\nMenu\n";
-        cout << "1. Enter First Polynominal\n";
-        cout << "2. Enter Second Polynominal\n";
-        cout << "3. Add Polynominals\n";
+        cout << "1. Enter First Polynomial\n";
+        cout << "2. Enter Second Polynomial\n";
+        cout << "3. Add Polynomials\n";
         cout << "4. Show Result\n";
         cout << "5. Exit\n";
         cout << "Enter your Choice: ";
         cin >> choice;
         if (choice == '1') {
-            poly_1 = input_polynominal();
-            cout << "First Polynominal entered: " << poly_1 << "\n";
+            poly_1 = inputPolynomial();
+            cout << "First Polynomial entered: " << poly_1 << "\n";
         }
         else if (choice == '2') {
-            poly_2 = input_polynominal();
-            cout << "Second Polynominal Entered: " << poly_2 << "\n";
+            poly_2 = inputPolynomial();
+            cout << "Second Polynomial Entered: " << poly_2 << "\n";
         }
         else if (choice == '3') {
             if (poly_1.head == nullptr || poly_2.head == nullptr) {
-                cout << "Please enter both polynominals first.\n";
+                cout << "Please enter both polynomials first.\n";
             }
             else {
                 result = poly_1 + poly_2;
-                cout << "Polynominals added.\n";
+                cout << "Polynomials added.\n";
             }
         }
         else if (choice == '4') {
             if (result.head == nullptr) {
-                cout << "No result to show. Please add polynominals first.\n";
+                cout << "No result to show. Please add polynomials first.\n";
             }
             else {
                 cout << "Result: " << result << "\n";
