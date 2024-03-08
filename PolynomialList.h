a73444dc9116ec54004aaefb4ef217b759a9782e
KatieD4 <162634216+KatieD4@users.noreply.github.com>
2024-03-07 20:42:26 -0600

    Create PolynomialList.h

---
+++ PolynomialList.h
@@ -0,0 +1,33 @@
+#ifndef POLYNOMIALLIST_H
+#define POLYNOMIALLIST_H
+
+#include <list>
+#include "Term.h"
+
+
+class PolynomialList {
+public:
+	// Constructor
+	PolynomialList() = default;
+
+	// Class-member functions.
+	// Function to input a polynomial.
+	void inputPolynomial();
+
+	// Function to add two polynomials and return the result.
+	PolynomialList addPolynomials(const PolynomialList& other) const;
+
+	// Function to display the result polynomial.
+	void displayResult() const;
+
+
+private:
+	std::list<Term> polynomial;
+
+	// Sort and combine like terms in the polynomial.
+	void sortAndCombineLikeTerms();
+};
+
+#endif
+
+
