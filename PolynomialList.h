d1398e66b7b26ba48e9464ac623258311d14e4b7
KatieD4 <162634216+KatieD4@users.noreply.github.com>
2024-03-08 22:44:16 -0600

    Update PolynomialList.h
    comments

--- PolynomialList.h
+++ PolynomialList.h
@@ -5,32 +5,37 @@
 #include "Term.h"
 
 
+// PolynomialList class represents a polynomial as a list of Terms.
 class PolynomialList {
 public:
-	// Constructor
+	// Default constructor. Initializes an empty polynomial list.
 	PolynomialList() = default;
 
 	// Class-member functions.
-	// Function to input a polynomial.
+	// Function to input a polynomial from the user.
+	// Reads a line of input and parses it into individual terms, which are then added to the polynomiallist.
 	void inputPolynomial();
 
-	// Function to add two polynomials and return the result.
+	// Function to add two polynomials and return the result as a new PolynomialList.
+	// other: the PolynomialList to be added to this one.
+	// Returns: a new PolynomialList representing the sum of first polynomial and "other".
 	PolynomialList addPolynomials(const PolynomialList& other) const;
 
-	// Function to display the result polynomial.
+	// Function to display the result polynomial in a user-friendly fashion.
+	// Fortmats and prints the polynomial terms to the console.
 	void displayResult() const;
 
 	// Function to check if polynomial is empty.
+	// Returns: {true} is the polynomial list is empty, {false} otherwise.
 	bool isEmpty() const;
 
-
 private:
+	// A doubly-linked list that stores the terms of the polynomial.
 	std::list<Term> polynomial;
 
-	// Sort and combine like terms in the polynomial.
+	// Sorts the polynomial terms in decreasing order of their exponents
+	// and combines like terms (terms with the same exponent) in the polynomial.
 	void sortAndCombineLikeTerms();
 };
 
 #endif
-
-
